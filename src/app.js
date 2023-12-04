65d24b4972080e12b66f801e27c8a5c640925290
Sohrab Hassibi <sohrab.hassibi@gmail.com>
2023-12-03 23:30:56 -0800

    UPDATE: port number to 8080

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const port = 8080;
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.listen(80, function () {
-  console.log("Example app listening on port 80!");
+app.post("/echo", function (req, res) {
+  console.log(`request = ${JSON.stringify(req.body, null, 4)}`);
+  res.send(req.body);
+});
+
+app.listen(port, function () {
+  console.log(`app listening on port ${port}!`);
 });
